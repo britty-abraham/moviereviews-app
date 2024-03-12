943f4ebe0caaf941231aaebd7f09999b43a3af11
20105650 <20105650@mail.wit.ie>
2024-03-12 10:56:46 +0000

    Initial commit

---
+++ test/moviereviews-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MoviereviewsApp from '../lib/moviereviews-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/moviereviews-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MoviereviewsApp.MoviereviewsAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
