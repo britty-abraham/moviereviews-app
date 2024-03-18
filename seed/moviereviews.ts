67d9947246f7ee28be4d3cf57f3d117d87234a3a
20105650 <20105650@mail.wit.ie>
2024-03-18 15:02:17 +0000

    Moviereview table content updated

--- seed/moviereviews.ts
+++ seed/moviereviews.ts
@@ -5,7 +5,7 @@ export const movieReviews: Moviereview[] = [
     MovieId: 111111,
     ReviewerName: 'TeenaAlex',
     ReviewDate: '2022-06-17',
-    Content: 'Recreating a timeless gem such as Steven Spielbergs 1985 film "The Color Purple" is undoubtedly a daunting endeavour, laden with serious challenges. However, the remake, sharing the same title, wisely chooses not to overshadow the original but rather honours it in a fresh and innovative way by opting for a musical path. Crafting a musical from such a dark subject matter inevitably presents challenges, yet this drama rises to the occasion. With a poignant tone, it explores turn-of-the-century America, where women endured oppression and were denied a voice of their own. Through its raw emotional portrayal, the film captures the gritty reality of a woman shattered by lifes hardships, yet clinging to a glimmer of hope for a brighter future.',
+    Content: 'Goblin Stone is a cute and engaging little game with a great message, all about taking the fight back against a bunch of XP-hungry bullies. It cleverly combines multiple genres into an amalgamation that works surprisingly well, making for a strategically engaging sim about saving Goblinkind.',
     Rating: 4
   },
   {
@@ -14,5 +14,19 @@ export const movieReviews: Moviereview[] = [
     ReviewDate: '2021-03-24',
     Content: 'Unlike the Miami transit service that gives the film its title and gets from first to final stop in just 16 minutes, Omni Loop takes time to wade through its tangled thicket of set-up and draw you in. But Bernardo Brittos near-future sci-fi — about death, time travel and the cherished gifts in life we take for granted while pursuing that elusive something more — sneaks up on you. The same goes for the expertly synced performances of Mary-Louise Parker, bringing her characteristic flinty authenticity to a role that could easily have drowned in quirk, and Ayo Edebiri, demonstrating once again that she’s in the top tier of emerging American actors.',
     Rating: 5
+},
+{
+  MovieId: 111111,
+  ReviewerName: 'DeepakDev',
+  ReviewDate: '2022-04-20',
+  Content: 'Goblin Stone has a repetitive but addictive gameplay loop with enough variants to make us want to keep you going. The twist in the narrative helps to keep the interest, despite a somewhat tedious progress system that does not fit with the narrative.',
+  Rating: 3
+},
+{
+  MovieId: 111111,
+  ReviewerName: 'AnnMaria',
+  ReviewDate: '2022-05-28',
+  Content: 'Goblin Stone tells the story of the poor goblins who defend themselves against the evil adventurers. The cute goblins have to position themselves well tactically and take part in various adventures to further expand the goblin base. A turn-based roguelike adventure with beautiful graphics and a great narrators voice.',
+  Rating: 4
 }
 ];
