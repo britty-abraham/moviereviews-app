c3820dfb25362d034e95b865118a0cfdc9a0fb87
20105650 <20105650@mail.wit.ie>
2024-03-22 14:17:04 +0000

    Add more data to import to table

--- seed/moviereviews.ts
+++ seed/moviereviews.ts
@@ -28,5 +28,26 @@ export const movieReviews: Moviereview[] = [
   ReviewDate: '2022-05-28',
   Content: 'Goblin Stone tells the story of the poor goblins who defend themselves against the evil adventurers. The cute goblins have to position themselves well tactically and take part in various adventures to further expand the goblin base. A turn-based roguelike adventure with beautiful graphics and a great narrators voice.',
   Rating: 4
-}
+},
+{
+  MovieId: 117811,
+  ReviewerName: 'Christian',
+  ReviewDate: '2022-05-01',
+  Content: 'You Can Call Me Bill isnt a travesty; hearing Shatner discuss his life is always fascinating. But instead, the films a missed opportunity to unpack one of the more enigmatic figures in our public consciousness.',
+  Rating: 3
+},
+{
+  MovieId: 117811,
+  ReviewerName: 'owen',
+  ReviewDate: '2023-07-01',
+  Content: 'The documentary captures how Shatner, as he began to make a career out of performing his public legend, merged his very identity with that of the hambone thespian inside him.',
+  Rating: 4
+},
+{
+  MovieId: 117811,
+  ReviewerName: 'DeepakDev',
+  ReviewDate: '2022-09-01',
+  Content: 'While fans might wish for a more traditional “follow me around” documentary that dives deep into all things Captain Kirk, this softer, more contemplative side is just as profound as the legacy he will leave behind.',
+  Rating: 2
+},
 ];
